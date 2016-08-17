db2629c6764dd0ca87cb2f0e6ae4a40efd636edd
prool <proolix@gmail.com>
2016-08-17 23:25:54 +0300

    Добавлена возможность получения сетов (наборов предметов) командой ДУХМАДА

---
+++ src/duhmada.c
@@ -0,0 +1,230 @@
+
+else if (!strcmp(argument,"0")) { // Броня
+PUT_OBJ( 68800 );
+PUT_OBJ( 68801 );
+PUT_OBJ( 68802 );
+PUT_OBJ( 68803 );
+PUT_OBJ( 68804 );
+PUT_OBJ( 68805 );
+PUT_OBJ( 68806 ); }
+else if (!strcmp(argument,"1")) { // Кожаный
+PUT_OBJ( 66804 );
+PUT_OBJ( 66805 );
+PUT_OBJ( 66806 );
+PUT_OBJ( 66807 );
+PUT_OBJ( 66808 );
+PUT_OBJ( 66809 );
+PUT_OBJ( 66810 );
+PUT_OBJ( 66811 ); }
+else if (!strcmp(argument,"2")) { // Доспехи
+PUT_OBJ( 1200 );
+PUT_OBJ( 1201 );
+PUT_OBJ( 1202 ); }
+else if (!strcmp(argument,"3")) { // Доспехи
+PUT_OBJ( 1203 );
+PUT_OBJ( 1204 );
+PUT_OBJ( 1205 ); }
+else if (!strcmp(argument,"4")) { // Доспехи
+PUT_OBJ( 1206 );
+PUT_OBJ( 1207 );
+PUT_OBJ( 1208 ); }
+else if (!strcmp(argument,"5")) { // Доспехи
+PUT_OBJ( 1209 );
+PUT_OBJ( 1210 );
+PUT_OBJ( 1211 );
+PUT_OBJ( 1212 ); }
+else if (!strcmp(argument,"6")) { // Доспехи
+PUT_OBJ( 1213 );
+PUT_OBJ( 1214 );
+PUT_OBJ( 1215 );
+PUT_OBJ( 1216 ); }
+else if (!strcmp(argument,"7")) { // Доспехи
+PUT_OBJ( 1217 );
+PUT_OBJ( 1218 );
+PUT_OBJ( 1219 );
+PUT_OBJ( 1220 );
+PUT_OBJ( 1221 ); }
+else if (!strcmp(argument,"8")) { // Снаряжение
+PUT_OBJ( 1222 );
+PUT_OBJ( 1223 );
+PUT_OBJ( 1224 );
+PUT_OBJ( 1225 );
+PUT_OBJ( 1226 );
+PUT_OBJ( 1227 );
+PUT_OBJ( 1228 );
+PUT_OBJ( 1229 );
+PUT_OBJ( 1230 );
+PUT_OBJ( 1231 );
+PUT_OBJ( 1232 );
+PUT_OBJ( 1233 );
+PUT_OBJ( 1234 );
+PUT_OBJ( 1235 );
+PUT_OBJ( 1236 );
+PUT_OBJ( 1237 ); }
+else if (!strcmp(argument,"9")) { // Облачение
+PUT_OBJ( 1238 );
+PUT_OBJ( 1239 );
+PUT_OBJ( 1240 );
+PUT_OBJ( 1241 );
+PUT_OBJ( 1242 );
+PUT_OBJ( 1243 );
+PUT_OBJ( 1244 );
+PUT_OBJ( 1245 );
+PUT_OBJ( 1246 );
+PUT_OBJ( 1247 );
+PUT_OBJ( 1248 );
+PUT_OBJ( 1249 );
+PUT_OBJ( 1250 );
+PUT_OBJ( 1251 );
+PUT_OBJ( 1252 );
+PUT_OBJ( 1253 ); }
+else if (!strcmp(argument,"10")) { // Одежды
+PUT_OBJ( 1254 );
+PUT_OBJ( 1255 );
+PUT_OBJ( 1256 );
+PUT_OBJ( 1257 );
+PUT_OBJ( 1258 );
+PUT_OBJ( 1259 );
+PUT_OBJ( 1260 );
+PUT_OBJ( 1261 );
+PUT_OBJ( 1262 );
+PUT_OBJ( 1263 );
+PUT_OBJ( 1264 );
+PUT_OBJ( 1265 );
+PUT_OBJ( 1266 );
+PUT_OBJ( 1267 ); }
+else if (!strcmp(argument,"11")) { // Доспехи
+PUT_OBJ( 3300 );
+PUT_OBJ( 3301 );
+PUT_OBJ( 3302 );
+PUT_OBJ( 3303 );
+PUT_OBJ( 3304 );
+PUT_OBJ( 3305 );
+PUT_OBJ( 3306 );
+PUT_OBJ( 3307 );
+PUT_OBJ( 3308 );
+PUT_OBJ( 3309 );
+PUT_OBJ( 3310 );
+PUT_OBJ( 3311 );
+PUT_OBJ( 3312 );
+PUT_OBJ( 3313 );
+PUT_OBJ( 3314 );
+PUT_OBJ( 3392 ); }
+else if (!strcmp(argument,"12")) { // Броня
+PUT_OBJ( 3315 );
+PUT_OBJ( 3316 );
+PUT_OBJ( 3317 );
+PUT_OBJ( 3318 );
+PUT_OBJ( 3319 );
+PUT_OBJ( 3320 );
+PUT_OBJ( 3321 );
+PUT_OBJ( 3322 );
+PUT_OBJ( 3323 );
+PUT_OBJ( 3324 );
+PUT_OBJ( 3325 );
+PUT_OBJ( 3326 );
+PUT_OBJ( 3327 );
+PUT_OBJ( 3328 );
+PUT_OBJ( 3359 ); }
+else if (!strcmp(argument,"13")) { // доспехи
+PUT_OBJ( 3329 );
+PUT_OBJ( 3330 );
+PUT_OBJ( 3331 );
+PUT_OBJ( 3332 );
+PUT_OBJ( 3333 );
+PUT_OBJ( 3334 );
+PUT_OBJ( 3335 );
+PUT_OBJ( 3336 );
+PUT_OBJ( 3337 );
+PUT_OBJ( 3338 );
+PUT_OBJ( 3339 );
+PUT_OBJ( 3340 );
+PUT_OBJ( 3341 );
+PUT_OBJ( 3342 );
+PUT_OBJ( 3357 );
+PUT_OBJ( 3358 ); }
+else if (!strcmp(argument,"14")) { // Одеяния
+PUT_OBJ( 3343 );
+PUT_OBJ( 3344 );
+PUT_OBJ( 3345 );
+PUT_OBJ( 3346 );
+PUT_OBJ( 3347 );
+PUT_OBJ( 3348 );
+PUT_OBJ( 3349 );
+PUT_OBJ( 3350 );
+PUT_OBJ( 3351 );
+PUT_OBJ( 3352 );
+PUT_OBJ( 3353 );
+PUT_OBJ( 3354 );
+PUT_OBJ( 3355 );
+PUT_OBJ( 3356 ); }
+else if (!strcmp(argument,"15")) { // Доспехи
+PUT_OBJ( 3360 );
+PUT_OBJ( 3361 );
+PUT_OBJ( 3362 );
+PUT_OBJ( 3363 );
+PUT_OBJ( 3364 );
+PUT_OBJ( 3365 );
+PUT_OBJ( 3366 );
+PUT_OBJ( 3367 );
+PUT_OBJ( 3368 );
+PUT_OBJ( 3369 );
+PUT_OBJ( 3370 );
+PUT_OBJ( 3371 );
+PUT_OBJ( 3372 );
+PUT_OBJ( 3373 );
+PUT_OBJ( 3374 );
+PUT_OBJ( 3391 ); }
+else if (!strcmp(argument,"16")) { // доспехи
+PUT_OBJ( 3375 );
+PUT_OBJ( 3376 );
+PUT_OBJ( 3377 );
+PUT_OBJ( 3378 );
+PUT_OBJ( 3379 );
+PUT_OBJ( 3380 );
+PUT_OBJ( 3381 );
+PUT_OBJ( 3382 );
+PUT_OBJ( 3383 );
+PUT_OBJ( 3384 );
+PUT_OBJ( 3385 );
+PUT_OBJ( 3386 );
+PUT_OBJ( 3387 );
+PUT_OBJ( 3388 );
+PUT_OBJ( 3389 );
+PUT_OBJ( 3390 ); }
+else if (!strcmp(argument,"17")) { // Доспех
+PUT_OBJ( 1268 );
+PUT_OBJ( 1269 );
+PUT_OBJ( 1270 );
+PUT_OBJ( 1271 ); }
+else if (!strcmp(argument,"18")) { // Новогодний
+PUT_OBJ( 2550 );
+PUT_OBJ( 2551 );
+PUT_OBJ( 2552 );
+PUT_OBJ( 2553 ); }
+else if (!strcmp(argument,"19")) { // Сотканные
+PUT_OBJ( 2554 );
+PUT_OBJ( 2555 );
+PUT_OBJ( 2556 );
+PUT_OBJ( 2557 ); }
+else if (!strcmp(argument,"20")) { // 
+PUT_OBJ( 2558 );
+PUT_OBJ( 2559 );
+PUT_OBJ( 2560 );
+PUT_OBJ( 2561 ); }
+else if (!strcmp(argument,"21")) { // 
+PUT_OBJ( 2562 );
+PUT_OBJ( 2563 );
+PUT_OBJ( 2564 );
+PUT_OBJ( 2565 ); }
+else if (!strcmp(argument,"22")) { // 
+PUT_OBJ( 2566 );
+PUT_OBJ( 2567 );
+PUT_OBJ( 2568 );
+PUT_OBJ( 2569 ); }
+else if (!strcmp(argument,"23")) { // 
+PUT_OBJ( 2570 );
+PUT_OBJ( 2571 );
+PUT_OBJ( 2572 );
+PUT_OBJ( 2573 ); }
+
